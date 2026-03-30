fc7593a0a8e80fe9d7f0d80ac1877efca21515ab
Natalie Sumbo Filipe <nataliesumbofilipe@MacBook-Pro-von-Natalie.local>
2026-03-30 13:29:47 +0200

    Clean project structure and update filenames

--- src/evaluation/metrics_retrieval.py
+++ src/evaluation/metrics_retrieval.py
@@ -18,17 +18,3 @@ def evaluate_retrieval(user_query, retrieved_chunks):
     )
     relevancy_metric.measure(test_case)
     return relevancy_metric.score, relevancy_metric.reason
-
-
-# def evaluate_embedding_model(user_query, retrieved_chunks):
-#     test_case = LLMTestCase(
-#         input=user_query,
-#         actual_output="N/A",  # dont need output of llm
-#         retrieval_context=retrieved_chunks,
-#     )
-#
-#     relevancy_metric.measure(test_case)
-#
-#     print(f"Contextual Relevancy Score: {relevancy_metric.score}")
-#     print(f"Reason: {relevancy_metric.reason}")
-#     return relevancy_metric.score
