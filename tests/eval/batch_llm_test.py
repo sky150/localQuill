275d2b9fba9f61ca2bcd6c277744d7d24675a1c3
SilasSchuler <silasphysik@gmail.com>
2026-05-05 13:06:51 +0200

    base_models with best model/temperature per category

--- tests/eval/batch_llm_test.py
+++ tests/eval/batch_llm_test.py
@@ -60,40 +60,40 @@ def get_fiction_dict():
     return fiction_dict
 
 if __name__ == "__main__":
-
     """This will run the full evaluation process for multiple models and multiple texts in a batch manner and add them to the eval_result.json"""
-    test_models = [
-        # "llama3.1",
-        # "mistral", # 7b 
-        # "ministral-3:8b",
-        # "mistral-nemo", 
-        # "qwen2.5:7b",
-        # "qwen2.5:14b",
-        # "qwen3:14b",
-        # # "qwen3.5:9b",  # Too slow gets stuck for fiction
-        # # "qwen3.6:27b",  # Too slow gets stuck for fiction
-        # "qwen3.5:4b", # Gets stuck loading forever. Too small to work with our context
-        # kimi 
-        # grok
-        # "mistral-small3.2:24b",
-        "qwen3:8b"
-    ] 
-    
-    ###
-
-    # Remove some items for testing
-    # essays.pop("Article_C2_Profishency_Response_3.txt")
-
     #####
     # The Following 3 Runs need to be strictly called by hand. 
     # They are batch calls, and will make countless LLM calls, so they run for ages.
     # Uncomment only one at a time
     #####
-    
+    # test_models = [
+    #     "llama3.1",
+    #     "mistral", # 7b 
+    #     "ministral-3:8b",
+    #     "mistral-nemo", 
+    #     "qwen2.5:7b",
+    #     "qwen2.5:14b",
+    #     "qwen3:8b",
+    #     "qwen3:14b",
+    #     "mistral-small3.2:24b",
+    #     "qwen3.5:9b",  # Too slow gets stuck for fiction
+    #     "qwen3.6:27b",  # Too slow gets stuck for fiction       
+    # ] 
     # essays = get_essay_dict()
     # result_file_name = "eval_generation_essay_results.jsonl"
     # test_run_full_evaluation(test_models, essays, style="essay", result_file_name=result_file_name)
     
+    
+    test_models = [
+        "qwen3:8b",
+        "llama3.1",
+        "mistral", # 7b 
+        "ministral-3:8b",
+        "mistral-nemo", 
+        "qwen2.5:7b",
+        "qwen2.5:14b",
+        "qwen3:14b",        
+    ] 
     fiction = get_fiction_dict()
     fiction.pop("LotR_Chapter_1_2_1000.txt")    # Only the shortest is actually used. The others are overwhelmed during g-eval
     fiction.pop("LotR_Chapter_1_3_2000.txt")
